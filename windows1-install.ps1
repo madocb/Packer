8e9b75338a972d9a6667640838ea02b024112784
Madoc <32127185+madocb@users.noreply.github.com>
2018-05-22 10:46:31 +0100

    Update windows1-install.ps1

--- windows1-install.ps1
+++ windows1-install.ps1
@@ -1,14 +1,19 @@
+# Install Chocolaty
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+# Install Python
 choco install -y python
 refreshenv
 $env:Path += ";C:\Python36;C:\Python36\Scripts"
+# Upgrade PIP
 python -m pip install --upgrade pip
 python -V
 pip -V
+# Install awscli
 pip install awscli
 aws --version
 mkdir s3-local
 cd s3-local
+# Copy all files from mys3packersrcbucket to cwd. 
+# AWS credentials are taken care of by the instance role
 aws s3 cp s3://mys3packersrcbucket/ ./ --recursive
-echo "#####################################"
 dir
